e1a1f7c0deb35efd952af7878703fd6615dd7ea3
Kare Morstol <kare@nottoobadsoftware.com>
2015-03-31 23:26:30 +0200

    Add more unit tests for token parser. Clean up the previous one.

--- tests/FootlessParserTests/Parser_Tests.swift
+++ tests/FootlessParserTests/Parser_Tests.swift
@@ -12,20 +12,41 @@ import XCTest
 
 class Parser_Tests: XCTestCase {
 
+	func testSingleTokenParser () {
+		let input = ParserInput([1])
+		let parser = token(1)
+
+		let result = parser.parse(input)
+		let (output, nextinput) = result.value!
+
+		XCTAssertEqual(output, 1)
+		XCTAssert( nextinput.next() == nil, "Input should be empty" )
+	}
+
 	func testSeveralTokenParsers () {
 		var input = ParserInput("abc")
 
 		for character in "abc" {
 			let parser = token(character)
 			let result = parser.parse(input)
-			if result.isSuccess {
-				input = result.value!.nextinput
-				XCTAssert(result.value!.output == character)
+			if let (output, nextinput) = result.value {
+				input = nextinput
+				XCTAssertEqual(output, character)
 			} else {
 				XCTFail("Parser failed with error message: " + result.error!)
 				return
 			}
 		}
 		XCTAssert( input.next() == nil, "Input should be empty" )
+	}
+
+	func testFailingParserReturnsError () {
+		let input = ParserInput([1])
+		let parser = token(2)
+
+		let result = parser.parse(input)
+
+		XCTAssertFalse(result.isSuccess)
+		XCTAssertFalse(result.error!.isEmpty, "Should have an error message")
 	}
 }
