b8e0a61e6a1c6ced4ed623f1bfab65ba97c29458
Kare Morstol <kare@nottoobadsoftware.com>
2015-04-09 19:32:18 +0200

    Test conformance to the associativity law.
    Finally. It certainly took me enough commits to get there.

--- tests/FootlessParserTests/Parser+Operators_Tests.swift
+++ tests/FootlessParserTests/Parser+Operators_Tests.swift
@@ -70,4 +70,22 @@ class FlatMap_Tests: XCTestCase {
 		assertParsesEqually(input: [1], leftside, rightside, shouldSucceed: true)
 		assertParsesEqually(input: [9], leftside, rightside, shouldSucceed: false)
 	}
+
+	// (m >>= f) >>= g = m >>= (\x -> f x >>= g)
+	func testAssociativityLaw () {
+		func timesTwo(x: Int) -> Parser<Int,Int> {
+			let x2 = x * 2
+			return satisfy(expect: "\(x2)") { $0 == x2 }
+		}
+
+		let leftside = (any() >>- token) >>- timesTwo
+		let rightside = any() >>- { token($0) >>- timesTwo }
+
+		assertParsesEqually(input: [1,1,2], leftside, rightside, shouldSucceed: true)
+		assertParsesEqually(input: [9,9,9], leftside, rightside, shouldSucceed: false)
+
+		let noparens = any() >>- token >>- timesTwo
+		assertParsesEqually(input: [1,1,2], leftside, noparens, shouldSucceed: true)
+		assertParsesEqually(input: [9,9,9], leftside, noparens, shouldSucceed: false)
+	}
 }
