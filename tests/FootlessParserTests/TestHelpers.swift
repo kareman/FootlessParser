2589ed6bfa41709ab92fb2b3a766b7d862e5f352
Kare Morstol <kare@nottoobadsoftware.com>
2015-06-06 16:12:54 +0200

    Add "pathForTestResource" test case function.

--- tests/FootlessParserTests/TestHelpers.swift
+++ tests/FootlessParserTests/TestHelpers.swift
@@ -88,3 +88,16 @@ extension XCTestCase {
 		assertParseFails(p, ParserInput(input), file: file, line: line)
 	}
 }
+
+
+import Foundation
+
+extension XCTestCase {
+
+	func pathForTestResource (filename: String, type: String) -> String {
+		let path = NSBundle(forClass: self.dynamicType).pathForResource(filename, ofType: type)
+		assert(path != nil, "resource \(filename).\(type) not found")
+		
+		return path!
+	}
+}
