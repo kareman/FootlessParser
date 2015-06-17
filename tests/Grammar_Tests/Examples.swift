05bfa31bfcbae11c29a627a381e5753254a686fc
Kare Morstol <kare@nottoobadsoftware.com>
2015-06-17 15:51:23 +0200

    Add example TestCase and example for parsing XML tags.
    Also disabled testParseCSVQuotesReturningArray because it takes so long.

---
+++ tests/Grammar_Tests/Examples.swift
@@ -0,0 +1,39 @@
+//
+// Examples.swift
+// FootlessParser
+//
+// Created by Kåre Morstøl on 17.06.15.
+// Copyright (c) 2015 NotTooBad Software. All rights reserved.
+//
+
+import FootlessParser
+import XCTest
+
+class Examples: XCTestCase {
+
+	func testXMLTagParser () {
+
+		func tuple <A,B> (a: A)(b: B) -> (A,B) {
+			return (a, b)
+		}
+
+		let opentag = char("<") *> oneOrMore(not(">")) <* char(">")
+		let closetag = { (tagname: String) in char("<") *> tokens(tagname) <* tokens("/>") }
+
+		let tag = tuple <^> opentag <*> oneOrMore(not("<")) >>- { (name, content) in
+			return { _ in (name, content) } <^> closetag(name)
+		}
+
+		// this, like so much else, will be far simpler in Swift 2. And prettier.
+		parse(tag, "<a>content<a/>").analysis(
+			ifSuccess: {(name, content) in
+				XCTAssertEqual(name, "a")
+				XCTAssertEqual(content, "content")
+			},
+			ifFailure: { XCTFail($0) }
+		)
+		assertParseFails(tag, input: "<a>content<b/>")
+		assertParseFails(tag, input: "a content<a/>")
+		assertParseFails(tag, input: "<a><a/>")
+	}
+}
