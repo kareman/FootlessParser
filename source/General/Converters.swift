322e1498f28944c1018ac1a0ea789ce5ad3b6a8e
Kare Morstol <kare@nottoobadsoftware.com>
2015-08-03 22:52:08 +0200

    Update for Xcode 7 beta 4.

--- source/General/Converters.swift
+++ source/General/Converters.swift
@@ -9,10 +9,10 @@
 
 /** Return a collection containing x and all elements of xs. Works with strings and arrays. */
 public func extend
-	<A, C: ExtensibleCollectionType where C.Generator.Element == A>
+	<A, C: RangeReplaceableCollectionType where C.Generator.Element == A>
 	(x: A)(xs: C) -> C {
 
-	// not satisfied with this way of doing it, but ExtensibleCollectionType has only mutable methods.
+	// not satisfied with this way of doing it, but RangeReplaceableCollectionType has only mutable methods.
 	var result = C()
 	result.append(x)
 	result.extend(xs)
@@ -21,7 +21,7 @@ public func extend
 
 /** Join 2 collections together. */
 public func extend
-	<C1: ExtensibleCollectionType, C2: CollectionType where C1.Generator.Element == C2.Generator.Element>
+	<C1: RangeReplaceableCollectionType, C2: CollectionType where C1.Generator.Element == C2.Generator.Element>
 	(var xs1: C1)(xs2: C2) -> C1 {
 
 	xs1.extend(xs2)
