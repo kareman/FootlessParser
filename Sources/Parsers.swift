7c14984542a104f82407a22621987be20780d38b
Bouke Haarsma <bouke@haarsma.eu>
2016-05-17 17:21:40 +0200

    Added standard set of string parsers

---
+++ Sources/Parsers.swift
@@ -0,0 +1,49 @@
+//
+// Parsers.swift
+// FootlessParser
+//
+// Released under the MIT License (MIT), http://opensource.org/licenses/MIT
+//
+// Copyright (c) 2016 Bouke Haarsma. All rights reserved.
+
+import Foundation
+
+/**
+ Parser that matches all the whitespace characters.
+
+ Matches the character set containing the characters in Unicode General 
+ Category Zs and CHARACTER TABULATION (U+0009).
+ */
+public let whitespace = char(NSCharacterSet.whitespaces(), name: "whitespace")
+
+/**
+ Parser that matches all the newline characters.
+
+ Matches the character set containing the newline characters (U+000A ~ U+000D, 
+ U+0085, U+2028, and U+2029).
+ */
+public let newline = char(NSCharacterSet.newlines(), name: "newline")
+
+/**
+ Parser that matches all the whitespace and newline characters.
+
+ Matches the character set containing characters in Unicode General Category 
+ Z*, U+000A ~ U+000D, and U+0085.
+ */
+public let whitespacesOrNewline = char(NSCharacterSet.whitespacesAndNewlines(), name: "whitespacesOrNewline")
+
+/**
+ Parser that matches all the decimal digit characters.
+
+ Matches the character set containing the characters in the category of Decimal
+ Numbers.
+ */
+public let digit = char(NSCharacterSet.decimalDigits(), name: "digit")
+
+/**
+ Parser that matches all the alphanumeric characters.
+
+ Matches the character set containing the characters in Unicode General 
+ Categories L*, M*, and N*.
+*/
+public let alphanumeric = char(NSCharacterSet.alphanumerics(), name: "alphanumeric")
